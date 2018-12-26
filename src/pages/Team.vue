296801467727fa1c214af9ce514d7baa2b56660f
ninarelvas <ninanic132@gmail.com>
2018-12-26 15:08:52 +0000

    pagina team terminada e responsiva com fotos temporarias

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -1,144 +1,389 @@
 <template>
     <div class="team-app">
-        <div class="slider-container">
-            <div class="image-slider">
-                <img class="images-title" src="https://www.projecttimes.com/media/k2/items/cache/6612da61425d98755836902a8bde1bce_XL.jpg">
-                <div class="title-team1">KNOW OUR</div>
-                <div class="title-team2">TEAM</div>
+        <div class="title-team-image">
+               <div class="title-team1">KNOW OUR<div class="title-team2">TEAM</div></div>
+        </div>
+       <div class="description-team">
+           <div class="team-text">
+                <div class="description-title">
+                    <span class="first-word-style"> JEEC'S</span> TEAM
+                </div>
+
+                <div class="description-text">Há 19 anos que trazemos ao Técnico os melhores
+                    empregadores de eletrotecnia!
+                </div>
             </div>
-            
-            <button class="prev"> < </button>
-            <button class="next" onclick="change_slide()"> ></button>
+            <img src="https://us.123rf.com/450wm/tupungato/tupungato1611/tupungato161100074/66488989-science-doodle-background-seamless-vector-texture-with-physics-concepts-.jpg?ver=6">
         </div>
 
-        <div class="description-team">
-            <div class="description-title">
-                <span class="first-word-style"> JEEC'S</span> TEAM
+        <div class="coordenacao">
+            <div class="title-div">COORDENAÇÃO</div>
+            <div class="team-flex">
+                <div class="team-member">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9f/Tom_Brady_2011.JPG/330px-Tom_Brady_2011.JPG">
+                    <p class="member-name">Ana Amaro</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/35/AaronRodgers.jpg/330px-AaronRodgers.jpg">
+                    <p class="member-name">José Correia</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Peyton_Manning_-_Broncos.jpg/330px-Peyton_Manning_-_Broncos.jpg">
+                    <p class="member-name">Maria Teles</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/JJ_Watt.JPG/330px-JJ_Watt.JPG">
+                    <p class="member-name">Rita Machado</p>
+                </div>            
             </div>
-            <div class="description-text">Há 19 anos que trazemos ao Técnico os melhores
-                empregadores de eletrotecnia!
-            </div>
-            <img class="image-description" src="https://us.123rf.com/450wm/tupungato/tupungato1611/tupungato161100074/66488989-science-doodle-background-seamless-vector-texture-with-physics-concepts-.jpg?ver=6">
         </div>
+
+        <div class="oradores">
+            <div class="title-div">CONTACTO EMPRESARIAL</div>
+            <div class="team-flex">
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/5/51/Bulbasaur.jpg/150px-Bulbasaur.jpg">
+                    <p class="member-name">Adelaide Ambrósio</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/f/f2/Charmander.jpg/150px-Charmander.jpg">
+                    <p class="member-name">André Pereira</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/f/f3/Squirtle.jpg/150px-Squirtle.jpg">
+                    <p class="member-name">Carolina Roque</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/a/af/Caterpie.jpg/150px-Caterpie.jpg">
+                    <p class="member-name">Catarina Pires</p>
+                </div> 
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/2/2a/Weedle.jpg/150px-Weedle.jpg">
+                    <p class="member-name">Gonçalo Morais</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/b/b1/Pidgey.jpg/150px-Pidgey.jpg">
+                    <p class="member-name">Ion Ciobotari</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/4/4b/Rattata.jpg/150px-Rattata.jpg">
+                    <p class="member-name">Isabel Castelo</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/d/de/Spearow.jpg/150px-Spearow.jpg">
+                    <p class="member-name">João Maria Janeiro</p>
+                </div>  
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/1/11/Ekans.jpg/150px-Ekans.jpg">
+                    <p class="member-name">Manuel Diniz</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/8/89/Pikachu.jpg/150px-Pikachu.jpg">
+                    <p class="member-name">Maria Salgueiro</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/2/2f/Sandshrew.jpg/150px-Sandshrew.jpg">
+                    <p class="member-name">Matilde Peixoto</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/9/9a/Nidoran-f.jpg/150px-Nidoran-f.jpg">
+                    <p class="member-name">Nuno Pavia</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/0/08/Nidoran-m.jpg/150px-Nidoran-m.jpg">
+                    <p class="member-name">Pedro Fernandes</p>
+                </div> 
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/b/b1/Clefairy.jpg/150px-Clefairy.jpg">
+                    <p class="member-name">Rui Paiva</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/a/ac/Vulpix.jpg/150px-Vulpix.jpg">
+                    <p class="member-name">Sérgio Marinheiro</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/8/83/Jigglypuff.jpg/150px-Jigglypuff.jpg">
+                    <p class="member-name">Tarcísio Silva</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/4/49/Oddish.jpg/150px-Oddish.jpg">
+                    <p class="member-name">Tiago Felizardo</p>
+                </div>  
+                <div class="team-member">
+                    <img src="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-lets-go-pikachu-eevee/thumb/9/96/Growlithe.jpg/150px-Growlithe.jpg">
+                    <p class="member-name">Vasco Araújo</p>
+                </div>                          
+            </div>
+        </div>
+
+         <div class="coordenacao">
+            <div class="title-div">MARKETING</div>
+            <div class="team-flex2">
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/608.jpg">
+                    <p class="member-name">Ana Silvestre</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/778.jpg">
+                    <p class="member-name">Ana Rita Pagaimo</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/795.jpg">
+                    <p class="member-name">Beatriz Silva</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/10547.jpg">
+                    <p class="member-name">Inês Coelho</p>
+                </div>  
+                 <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/2268.jpg">
+                    <p class="member-name">Madalena Ramos</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/649.jpg">
+                    <p class="member-name">Sara Bernardes</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://static.thetoptens.com/img/lists/19090.jpg">
+                    <p class="member-name">Tiago Silvério</p>
+                </div>                 
+            </div>
+        </div>
+
+        <div class="oradores">
+            <div class="title-div">ORADORES</div>
+            <div class="team-flex">
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501585326/vsaa-angel-adriana-1.jpg">
+                    <p class="member-name">Bárbara Correia</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501605465/vsaa-angel-alessandra-1.jpg">
+                    <p class="member-name">Bernardo Gonilho</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501612880/vsaa-angel-behati-1.jpg">
+                    <p class="member-name">Duarte Honrado</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501627310/vsaa-angel-candice-1.jpg">
+                    <p class="member-name">Pedro Martins</p>
+                </div> 
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501637513/vsaa-angel-elsa-1.jpg">
+                    <p class="member-name">Sara Barroso</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501655565/vsaa-angel-lily-1.jpg">
+                    <p class="member-name">Sofia Bebiano</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501650882/vsaa-angel-jasmine-1.jpg">
+                    <p class="member-name">Sophie Taboada</p>
+                </div>
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501648264/vsaa-angel-josephine.jpg">
+                    <p class="member-name">Teresa Araújo</p>
+                </div>  
+                <div class="team-member">
+                    <img src="https://dm.victoriassecret.com/imagefeatures/0x0/1267501657903/vsaa-angel-sara-1.jpg">
+                    <p class="member-name">Tiago Brito</p>
+                </div>             
+            </div>
+        </div>
+
+         <div class="coordenacao">
+            <div class="title-div">DEVELOPMENT</div>
+            <div class="team-flex2">
+                <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_01.jpg">
+                    <p class="member-name">Carlos Silva</p>
+                </div>
+                <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_02.jpg">
+                    <p class="member-name">Gilberto Silva</p>
+                </div>
+                <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_04.jpg">
+                    <p class="member-name">Guilherme Lopes</p>
+                </div>
+                <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_05.jpg">
+                    <p class="member-name">Ivan Andrushka</p>
+                </div>  
+                 <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_03.jpg">
+                    <p class="member-name">Marina Relvas</p>
+                </div>
+                <div class="team-member">
+                    <img src="http://a.espncdn.com/prod/styles/pagetype/otl/20180606_worldcuprank/images/wcr_action_07.jpg">
+                    <p class="member-name">Raúl Vaqueiro</p>
+                </div>                
+            </div>
+        </div>
+        
 
         <contacts/>
     </div>
 </template>
 
-<script>
-var x = document.getElementsByClassName("image-slider");
-var t = x.length;
 
-var slide_selected=0;
-function change_slide(){
-    if(slide_selected<t){
-        slide_selected++;
-    }
-    else{slide_selected=0;}
-    for(i=0;i<t;i++){
-        if(i=slide_selected){
-            x[i].style.display="initial";
-        }
-        else{
-            x[i].style.display="none";
-        }
-    }
-}
-
-</script>
 
 
 <style>
-.slider-container{
-    height: 300px;
-}
 
-.image-slider{
-    display: initial;
-    
-}
+.title-team-image{
+    background: linear-gradient(122deg, rgba(107,191, 255, 0.7), rgba(63, 0, 70, 0.7)), url("https://www.projecttimes.com/media/k2/items/cache/6612da61425d98755836902a8bde1bce_XL.jpg");
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 400px;
 
+
+}
 .title-team1{
-    position: absolute;
-    top: 20%;
-    left: 40%;
+    text-align: center;
+    position: relative;
+    top: 45%;
     color: #ffffff;
     font-family:'Raleway' sans-serif;
-    font-size: 30px;
-    text-shadow: 2px 2px 4px black;
+    font-size: 35px;
+    letter-spacing: -1px;
 }
+
 .title-team2{
-    position: absolute;
-    top: 27%;
-    left: 43%;
-    color: #ffffff;
-    font-family:'Raleway' sans-serif;
-    font-size: 40px;
+    font-size: 60px;
     font-weight: bold;
-    text-shadow: 2px 2px 4px black;
 }
 
-.images-title{
-    width: 100%;
-    height: 300px;
-    filter: blur(0.7px);
-
-}
-
-button{
-    padding: 20px;
-    border: none;
-    cursor: pointer;
-    font-size: 20px;
-    color: white;
-    background-color: transparent;
-
-}
-.next{
-    position: absolute;
-    top: 20%;
-    left:92%;
-}
-
-.prev{
-    position: absolute;
-    top: 20%;
-    left: 1%;
-}
 
 .description-team{
-    height: 250px;
+    height: 300px;
     background-color:white;
+    position: relative;
 }
+
+.description-team img{
+    width: 600px;
+    transform: rotate(30deg);
+    clip-path: polygon(0 0, 0 57.7%, 100% 0);
+    position: absolute;
+    top:110px;
+    left: -200px;
+    z-index: 0;
+}
+
+.team-text{
+    float: right;
+    padding-right: 40px;
+    position: relative;
+    z-index: 1;
+}
+
 
 .description-title{
     font-family: 'Raleway', sans-serif;
-    float: right;
-    padding-top: 50px;
-    padding-right: 20px;
-    font-size: 35px;
+    font-size: 50px;
+    font-weight: bolder;
     color: #505050;
-    font-weight:900;
+    text-align: right;
+    padding-top: 30px;
     }
 
 .description-text{
+    font-size: 30px;
+    width: 600px;
     text-align: right;
+    color: #505050;
+    padding-top: 20px;
+}
+
+.coordenacao{
+    height: 100%;
+    background-color: #ecf7ff;
+}
+
+.title-div{
+    font-family: 'Raleway', sans-serif;
+    font-weight: bolder;
+    color: #1d67d5;
+    font-size: 60px;
+    letter-spacing: -1px;
+    padding-top: 30px;
+}
+
+.team-flex{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin-right: 115px;
+    margin-left: 115px;
+    margin-top: 30px;
+}
+
+
+.team-flex img{
+    border-radius: 100%;
+    height: 220px;
+    width: 220px;
+    margin-top: 30px;
+}
+.team-flex2{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    margin-right: 250px;
+    margin-left: 250px;
+    margin-top: 30px;
+}
+
+.team-flex2 img{
+    border-radius: 100%;
+    height: 220px;
+    width: 220px;
+    margin-top: 30px;
+}
+
+.member-name{
+    width: 120px;
     font-size: 20px;
-    width: 400px;
-    position: relative;
-    top: 40%;
-    left: 44%;
-
-
+    text-align: center;
+    font-family:'Raleway', sans-serif;
+    color: #262626;
+    margin-left: 50px;
 }
 
-.image-description{
-    transform: rotate(30deg);
-    position: absolute;
-    top: 61.8%;
-    left: -20%;
-    overflow: hidden;
-    clip-path: polygon(0 0, 0 57.7%, 100% 0);
+.oradores{
+    background-color: #eeeeee;
+    height: 100%;
 }
+@media screen and (max-width: 900px) {
+    .description-title{
+        font-size: 40px;
+    }
+    .description-text{
+        font-size: 20px;
+        width: 400px;
+    }
 
+    .description-team img{
+        width: 500px;
+        top: 142px;
+    }
+    .title-div{
+        font-size: 40px;
+    }
+    .team-flex, .team-flex2{
+        margin-left: 50px;
+        margin-right: 50px;
+
+    }
+    .team-flex img, .team-flex2 img{
+        width: 180px;
+        height: 180px;
+    }
+    .member-name{
+        font-size: 20px;
+        margin-left:30px;
+    }
+}
 </style>
