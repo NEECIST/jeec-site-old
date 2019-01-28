cb527dced5a6941b78a7e1a0c5be2a9242669857
JosephTheCoder <menesesjmmc@gmail.com>
2019-01-28 19:06:53 +0000

    Fixed 404 route

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -13,24 +13,24 @@ export default new Router({
   scrollBehavior() {
     return { x: 0, y: 0 };
   },
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
       component: Home
     },
-    /*{
-      path: '/partners',
-      name: 'Partners',
-      component: Partners
-    },*/
     { 
       path: '/404', 
       component: Error_Handling 
     },  
     { path: '*', 
       redirect: '/404' 
-    },  
-  ],
-  mode: 'history'
+    },
+    /*{
+      path: '/partners',
+      name: 'Partners',
+      component: Partners
+    },*/  
+  ]
 })
