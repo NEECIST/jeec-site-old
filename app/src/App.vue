52f4dde3dea4768a72d2b9f61092491e7c0ee79d
JosephTheCoder <menesesjmmc@gmail.com>
2019-02-10 21:49:00 +0000

    changed background color to white

--- app/src/App.vue
+++ app/src/App.vue
@@ -17,7 +17,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #ffffff;
+  color: #2c3e50;
   width: 100%;
   background-color: white;
 }
