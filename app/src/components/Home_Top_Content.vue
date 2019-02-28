9134ada51ac75594335bf666ba6d9969148ed06f
JosephTheCoder <menesesjmmc@gmail.com>
2019-02-28 15:28:14 +0000

    Changed date format

--- app/src/components/Home_Top_Content.vue
+++ app/src/components/Home_Top_Content.vue
@@ -33,7 +33,7 @@ export default {
   data() {
     return {
 
-      date: "11 to 15th March",
+      date: "March 11 - 15",
       location: "Instituto Superior Técnico",
       social_media_text: "Follow us on",
       center_image_link: "../../static/jeec_logo.svg",
